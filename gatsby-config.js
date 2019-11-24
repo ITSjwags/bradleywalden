69e7e52af14036821c21834de7d73ea6616219e4
ITSjwags <jvwagoner@gmail.com>
2019-11-23 19:17:13 -0800

    Fix build

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,16 +7,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `Playfair\:700`,
-          `Poppins\:200,400,700`
-        ]
-      }
+        fonts: [`Playfair\:700`, `Poppins\:200,400,700`],
+      },
     },
     {
-      resolve: "gatsby-plugin-postcss-sass",
+      resolve: 'gatsby-plugin-postcss-sass',
       options: {
-        postCssPlugins: [require('autoprefixer')()]
+        postCssPlugins: [require('autoprefixer')()],
       },
     },
     'gatsby-plugin-react-helmet',
@@ -24,30 +21,18 @@ module.exports = {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: `${__dirname}/src/images`
-        }
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "img",
-        path: `${__dirname}/src/images`
-      }
-    },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
-    {
-      resolve: `gatsby-plugin-netlify`,
-      options: {
-        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+          include: `${__dirname}/src/images`,
+        },
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'img',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
   ],
 }
