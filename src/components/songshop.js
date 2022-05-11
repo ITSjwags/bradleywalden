47aa0908c3ea61fc908ae8cf35189cc913f61686
ITSjwags <jvwagoner@gmail.com>
2022-05-11 16:55:26 -0700

    remove simple song from songshop

--- src/components/songshop.js
+++ src/components/songshop.js
@@ -22,38 +22,6 @@ const SongShopPage = ({ active }) => (
     <p></p>
 
     <p className="Songwriting-title">
-      <span>Simple Song: $100</span>
-      <br />A simple piano/acoustic style recording.
-    </p>
-
-    {/* <p className="text-warn">SOLD OUT</p> */}
-
-    <form
-      action="https://www.paypal.com/cgi-bin/webscr"
-      method="post"
-      target="_top"
-    >
-      <input type="hidden" name="cmd" value="_s-xclick" />
-      <input type="hidden" name="hosted_button_id" value="YW3SZQRSGMMJE" />
-      <input
-        className="buynow"
-        type="image"
-        src="http://squidthewhale.com/buynow.png"
-        name="submit"
-        alt="PayPal - The safer, easier way to pay online!"
-      />
-      <img
-        alt=""
-        src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
-        width="1"
-        height="1"
-      />
-    </form>
-
-    <p></p>
-    <p></p>
-
-    <p className="Songwriting-title">
       <span>Full Song: $200</span>
       <br />A full length piano/acoustic style recording.
     </p>
